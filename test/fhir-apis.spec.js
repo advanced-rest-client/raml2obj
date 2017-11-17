6124c61fa05a943eef3ec638cf1f3c52d7b7dd3b
Pawel Psztyc <jarrodek@gmail.com>
2017-11-16 17:19:06 -0800

    New: Added new test of very complex API

---
+++ test/fhir-apis.spec.js
@@ -0,0 +1,50 @@
+const raml2obj = require('..');
+const {assert} = require('chai');
+const parser = require('./parser');
+const {performance} = require('perf_hooks');
+
+describe('raml2obj', () => {
+  describe('RAML example API (ARC)', function() {
+    this.timeout(100000);
+    var obj;
+
+    before(() => {
+      performance.mark('Start');
+      return parser('test/fhir-apis/healthcare-system-api.raml')
+      .then(result => {
+        performance.mark('Parser end');
+        performance.mark('Expand');
+        return raml2obj.parse({
+          json: result
+        });
+      })
+      .then(result => {
+        performance.mark('Expand end');
+        obj = result.json;
+
+        performance.measure('Parsing time', 'Start', 'Parser end');
+        performance.measure('Expanding time', 'Expand', 'Expand end');
+        performance.measure('Overall time', 'Start', 'Expand end');
+      });
+    });
+
+    it('Should be parsed', function() {
+      assert.typeOf(obj, 'object');
+    });
+
+    it('Should parse it in time', function() {
+      const measure = performance.getEntriesByName('Parsing time')[0];
+      assert.isBelow(measure.duration, 10000);
+    });
+
+    it('Should expand it in time', function() {
+      const measure = performance.getEntriesByName('Expanding time')[0];
+      assert.isBelow(measure.duration, 20000);
+    });
+
+    it('Should finish in time', function() {
+      const measure = performance.getEntriesByName('Overall time')[0];
+      assert.isBelow(measure.duration, 30000);
+    });
+  });
+});
